f89892d697e3bc3b856dc3068505c7fee5cb1728
Mark Verma <78155861+markvrma@users.noreply.github.com>
2023-08-01 00:09:09 +0530

    added more comments

--- color_extract/extract_color.py
+++ color_extract/extract_color.py
@@ -9,9 +9,17 @@ def extract_colors(img):
     '''
 
     image = cv2.imdecode(np.fromstring(img.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+
+    # convert rgb to grayscale 
+    # preparing for thresholding
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # using otsu thresholding
+    # creating inverted binary image
     _, threshold = cv2.threshold(
         gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+    #finding contours from the binary image
     contours, _ = cv2.findContours(
         threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -22,10 +30,17 @@ def extract_colors(img):
             contours, key=cv2.contourArea, reverse=True)[:10]
         sorted_contours = sorted(
             sorted_contours, key=lambda c: cv2.boundingRect(c)[0])
-
+        
+        # for each contour
         for i, contour in enumerate(sorted_contours):
+            # geting x,y coordinates and width and height
+            # for each rectangle and extracting AOI from the 
+            # original RGB image
             x, y, w, h = cv2.boundingRect(contour)
             color_roi = image[y:y + h, x:x + w]
+
+            # extracting the color from the first
+            # pixel of a rectangle
             pixel_color = color_roi[0, 0]
 
             color_name = ['URO', 'BIL', 'KET', 'BLD',
